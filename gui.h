b0e3077d84e8b2a9c305a16421a912ff3d95a576
Henrik Grubbström (Grubba) <grubba@grubba.org>
1996-07-11 22:14:21 +0200

    Added gui_AddButton().
    Rev: gui.h:1.2

--- gui.h
+++ gui.h
@@ -1,9 +1,13 @@
 /*
- * $Id: gui.h,v 1.1 1996/07/11 15:38:57 grubba Exp $
+ * $Id: gui.h,v 1.2 1996/07/11 20:14:21 grubba Exp $
  *
  * User Interface for the M68000 to Sparc recompiler
  *
- * $Log$
+ * $Log: gui.h,v $
+ * Revision 1.1  1996/07/11 15:38:57  grubba
+ * Graphics User-Interface files.
+ * Initial version.
+ *
  *
  */
 
@@ -37,6 +41,11 @@ Window gui_CreateSimpleWindow(Display *display, Window root,
 			      unsigned long border_color, unsigned long background);
 int gui_AllocColor(Display *display, Colormap cmap,
 		   int red, int green, int blue, int fallback);
+void gui_AddButton(const char *label, void (*callback)(void *), void *callbackarg,
+		   int x, int y, unsigned width, unsigned height,
+		   Window window, GC gc, XFontStruct *font,
+		   int backgroundPixel, int shadowPixel);
+
 struct my_x_string *new_my_x_string(const char *string, int x, int y,
 				    GC gc, struct my_x_string *last_str);
 GC my_create_font_gc(Display *display, int screen_num, Window window,
