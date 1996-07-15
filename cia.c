afaba49ca9370b5eb98f05c642207ce81184de66
Henrik Grubbström (Grubba) <grubba@grubba.org>
1996-07-15 07:20:57 +0200

    Added timer A and B. Real-time timers now go at 1/10th of real-speed.
    Rev: cia.c:1.2

--- cia.c
+++ cia.c
@@ -1,9 +1,12 @@
 /*
- * $Id: cia.c,v 1.1 1996/07/15 02:50:53 grubba Exp $
+ * $Id: cia.c,v 1.2 1996/07/15 05:20:57 grubba Exp $
  *
  * CIA emulation.
  *
- * $Log$
+ * $Log: cia.c,v $
+ * Revision 1.1  1996/07/15 02:50:53  grubba
+ * Cleaned up CIA emulation, and put it in a separate file.
+ *
  *
  */
 
@@ -73,6 +76,10 @@ void cia_write_reg(BYTE regnum, BYTE val)
  */
 
 /*
+ * NOTE: Real time timers go a factor 10 slower than the real thing!
+ */
+
+/*
  * CIA A
  */
 
@@ -116,6 +123,78 @@ void ciaa_write_ddra(BYTE regnum, BYTE val)
 }
 
 /*
+ * CIA A: Timers
+ */
+
+static int ciaa_timera_latch = 0;
+static int ciaa_timera_offset = 0;
+hrtime_t ciaa_timera_base = 0;
+
+BYTE ciaa_read_timera(BYTE reg)
+{
+  if ((cia_registers[0x0e] & 0x20) || (!(cia_registers[0x0e] & 0x01))) {
+    /* CNT line */
+    printf("Other timermodes not supported!\n");
+
+    return (((BYTE *)&ciaa_timera_offset)[7 - reg]);
+  } else {
+    /* 10s of CPU cycles */
+    hrtime_t current = gethrtime() - ciaa_timera_base;
+    int value = ciaa_timera_offset - ((current * 709379) / 10000000000LL);
+    if (value < 0) {
+      value = 0;
+    }
+
+    printf("CIA A: Timer A is 0x%04x\n", value);
+
+    return (((BYTE *)&value)[7-reg]);
+  }
+}
+
+void ciaa_write_timera(BYTE reg, BYTE val)
+{
+  ((BYTE *)&ciaa_timera_latch)[7 - reg] = val;
+  ((BYTE *)&ciaa_timera_offset)[7 - reg] = val;
+  ciaa_timera_base = gethrtime();
+}
+
+/*
+ * CIA A: Timer B
+ */
+
+static int ciaa_timerb_latch = 0;
+static int ciaa_timerb_offset = 0;
+hrtime_t ciaa_timerb_base = 0;
+
+BYTE ciaa_read_timerb(BYTE reg)
+{
+  if ((cia_registers[0x0f] & 0x30) || (!cia_registers[0x0f] & 0x01)) {
+    /* CNT or Timer A or Timer A and CNT */
+    printf("Other timermodes not supported!\n");
+
+    return (((BYTE *)&ciaa_timerb_offset)[9 - reg]);
+  } else {
+    /* 10s of CPU cycles */
+    hrtime_t current = gethrtime() - ciaa_timerb_base;
+    int value = ciaa_timerb_offset - ((current * 709379) / 10000000000LL);
+    if (value < 0) {
+      value = 0;
+    }
+    
+    printf("CIA A: Timer B is 0x%04x\n", value);
+
+    return (((BYTE *)&value)[9-reg]);
+  }
+}
+
+void ciaa_write_timerb(BYTE reg, BYTE val)
+{
+  ((BYTE *)&ciaa_timerb_latch)[9 - reg] = val;
+  ((BYTE *)&ciaa_timerb_offset)[9 - reg] = val;
+  ciaa_timerb_base = gethrtime();
+}
+
+/*
  * CIA A: Time of day clock
  */
 
@@ -129,7 +208,7 @@ BYTE ciaa_read_tod(BYTE reg)
   if (!ciaa_tod_state) {
     hrtime_t current = gethrtime() - ciaa_tod_base;
 
-    ciaa_tod_value = (current / 5000000) + ciaa_tod_offset;
+    ciaa_tod_value = (current / 50000000) + ciaa_tod_offset;
   }
   if (reg == 0x0a) {
     ciaa_tod_state |= 1;
@@ -164,8 +243,58 @@ void ciaa_write_alarm(BYTE reg, BYTE val)
  * CIA A: Control registers
  */
 
+void ciaa_write_cra(BYTE regnum, BYTE val)
+{
+  BYTE bits = cia_registers[regnum] ^ val;
+
+  cia_registers[regnum] = val;
+
+  if (val & 0x10) {
+    /* Load Timer A */
+
+    ciaa_timera_offset = ciaa_timera_latch;
+  }
+  if (bits & 0x01) {
+    if (val & 0x01) {
+      /* Start Timer A */
+      ciaa_timera_base = gethrtime();
+    } else {
+      /* Stop Timer A */
+      hrtime_t current = gethrtime() - ciaa_timera_base;
+
+      ciaa_timera_offset -= ((current * 709379) / 100000000000LL);
+      if (ciaa_timera_offset < 0) {
+	ciaa_timera_offset = 0;
+      }
+    }
+  }
+}
+
 void ciaa_write_crb(BYTE regnum, BYTE val)
 {
+  BYTE bits = cia_registers[regnum] ^ val;
+
+  cia_registers[regnum] = val;
+
+  if (val & 0x10) {
+    /* Load Timer B */
+
+    ciaa_timerb_offset = ciaa_timerb_latch;
+  }
+  if (bits & 0x01) {
+    if (val & 0x01) {
+      /* Start Timer B */
+      ciaa_timerb_base = gethrtime();
+    } else {
+      /* Stop Timer B */
+      hrtime_t current = gethrtime() - ciaa_timerb_base;
+
+      ciaa_timerb_offset -= ((current * 709379) / 10000000000LL);
+      if (ciaa_timerb_offset < 0) {
+	ciaa_timerb_offset = 0;
+      }
+    }
+  }
   if (val & 0x80) {
     /* ALARM */
     cia_write_tab[0x08] = ciaa_write_alarm;
@@ -189,10 +318,10 @@ static BYTE (*cia_read_tab[])(BYTE) = {
   cia_read_bad,	/* PRB */
   cia_read_reg,	/* DDRA */
   cia_read_bad,	/* DDRB */
-  cia_read_bad,	/* TALO */
-  cia_read_bad,	/* TAHI */
-  cia_read_bad,	/* TBLO */
-  cia_read_bad,	/* TBHI */
+  ciaa_read_timera,	/* TALO */
+  ciaa_read_timera,	/* TAHI */
+  ciaa_read_timerb,	/* TBLO */
+  ciaa_read_timerb,	/* TBHI */
   ciaa_read_tod,	/* TODLO */
   ciaa_read_tod,	/* TODMID */
   ciaa_read_tod,	/* TODHI */
@@ -216,8 +345,8 @@ static BYTE (*cia_read_tab[])(BYTE) = {
   cia_read_bad,	/* TODHR */
   cia_read_bad,	/* SDR */
   cia_read_bad,	/* ICR */
-  cia_read_bad,	/* CRA */
-  cia_read_bad,	/* CRB */
+  cia_read_reg,	/* CRA */
+  cia_read_reg,	/* CRB */
 };
 
 static void (*cia_write_tab[])(BYTE, BYTE) = {
@@ -226,17 +355,17 @@ static void (*cia_write_tab[])(BYTE, BYTE) = {
   cia_write_bad,	/* PRB */
   ciaa_write_ddra,	/* DDRA */
   cia_write_bad,	/* DDRB */
-  cia_write_bad,	/* TALO */
-  cia_write_bad,	/* TAHI */
-  cia_write_bad,	/* TBLO */
-  cia_write_bad,	/* TBHI */
+  ciaa_write_timera,	/* TALO */
+  ciaa_write_timera,	/* TAHI */
+  ciaa_write_timerb,	/* TBLO */
+  ciaa_write_timerb,	/* TBHI */
   ciaa_write_tod,	/* TODLO */
   ciaa_write_tod,	/* TODMID */
   ciaa_write_tod,	/* TODHI */
   cia_write_bad,	/* TODHR */
   cia_write_bad,	/* SDR */
   cia_write_bad,	/* ICR */
-  cia_write_bad,	/* CRA */
+  ciaa_write_cra,	/* CRA */
   ciaa_write_crb,	/* CRB */
   /* CIA B */
   cia_write_bad,	/* PRA */
