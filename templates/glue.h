9fb3337a1dbf3fefb223ea710e5c06839bf0d3fc
Henrik Grubbström (Grubba) <grubba@grubba.org>
1996-08-10 20:52:08 +0200

    Some more tastes of s_fix_sr. Now with s_sr_post_tab (table of post instruction SR fixup templates).
    Rev: templates/glue.gasp:1.3
    Rev: templates/glue.h:1.3

--- templates/glue.h
+++ templates/glue.h
@@ -1,9 +1,13 @@
 /*
- * $Id: glue.h,v 1.2 1996/07/17 16:02:50 grubba Exp $
+ * $Id: glue.h,v 1.3 1996/08/10 18:52:08 grubba Exp $
  *
  * Headerfile for the M68000 to Sparc recompiler.
  *
  * $Log: glue.h,v $
+ * Revision 1.2  1996/07/17 16:02:50  grubba
+ * Changed from {U,}{LONG,WORD,BYTE} to [SU]{8,16,32}.
+ * Hopefully all places got patched.
+ *
  * Revision 1.1.1.1  1996/06/30 23:51:55  grubba
  * Entry into CVS
  *
@@ -19,6 +23,8 @@
 /*
  * Assembler glue template code
  */
+
+extern U32 *s_sr_post_tab[];
 
 extern U32 s_pre_amble[];
 extern U32 s_raise_exception[];
